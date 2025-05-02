5e3c1fdacdfb8aa550f7667d01b89726ff5d9b0a
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-05-02 12:56:54 +0530

    grep Story 1 related code added

---
+++ grep-tool/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	const (
+		successCode int = 0
+		errorCode   int = 1
+	)
+
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <search_string> <filename>\n", os.Args[0])
+		os.Exit(errorCode)
+	}
+
+	searchString := os.Args[1]
+	filename := os.Args[2]
+
+	file, err := validateFile(filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(errorCode)
+	}
+	defer file.Close()
+
+	matches, err := grepFile(searchString, file)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s: %v\n", os.Args[0], filename, err)
+		os.Exit(errorCode)
+	}
+
+	for _, matchedLines := range matches {
+		fmt.Println(matchedLines)
+	}
+
+	os.Exit(successCode)
+}
+
+func validateFile(filename string) (*os.File, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		if os.IsPermission(err) {
+			return nil, fmt.Errorf("%s: %s: Permission denied", os.Args[0], filename)
+		}
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("%s: %s: open: No such file or directory", os.Args[0], filename)
+		}
+		return nil, fmt.Errorf("%s: %s: %v", os.Args[0], filename, err)
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return nil, fmt.Errorf("%s: %s: %v", os.Args[0], filename, err)
+	}
+
+	if info.IsDir() {
+		file.Close()
+		return nil, fmt.Errorf("%s: %s: read: Is a directory", os.Args[0], filename)
+	}
+
+	return file, nil
+}
+
+func grepFile(searchString string, reader io.Reader) ([]string, error) {
+	var matches []string
+
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, searchString) {
+			matches = append(matches, line)
+		}
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return matches, nil
+}
