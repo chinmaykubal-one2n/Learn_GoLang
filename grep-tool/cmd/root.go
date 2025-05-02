87d7912980863368617b08c35ac8daf4df7ff963
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-05-02 17:31:07 +0530

    cobra libray added

---
+++ grep-tool/cmd/root.go
@@ -0,0 +1,144 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var outFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "./mygrep <search_string> <filename> [-o out.txt]",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		searchString := args[0]
+		var reader io.Reader
+
+		switch len(args) {
+		case 1:
+			reader = os.Stdin
+
+		case 2:
+			filename := args[1]
+			file, err := validateFile(filename)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			defer file.Close()
+			reader = file
+		}
+
+		matches, err := grepReader(searchString, reader)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+			os.Exit(1)
+		}
+
+		if err := writeOutput(matches, outFile); err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+			os.Exit(1)
+		}
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&outFile, "out", "o", "", "Write output to file instead of stdout")
+}
+
+func validateFile(filename string) (*os.File, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		if os.IsPermission(err) {
+			return nil, fmt.Errorf("%s: %s: Permission denied", os.Args[0], filename)
+		}
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("%s: %s: open: No such file or directory", os.Args[0], filename)
+		}
+		return nil, fmt.Errorf("%s: %s: %v", os.Args[0], filename, err)
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return nil, fmt.Errorf("%s: %s: %v", os.Args[0], filename, err)
+	}
+
+	if info.IsDir() {
+		file.Close()
+		return nil, fmt.Errorf("%s: %s: read: Is a directory", os.Args[0], filename)
+	}
+
+	return file, nil
+}
+
+func grepReader(searchString string, reader io.Reader) ([]string, error) {
+	var matches []string
+
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, searchString) {
+			matches = append(matches, line)
+		}
+	}
+
+	err := scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return matches, nil
+}
+
+func writeOutput(lines []string, outPath string) error {
+	if outPath == "" {
+		for _, line := range lines {
+			fmt.Println(line)
+		}
+		return nil
+	}
+
+	if _, err := os.Stat(outPath); err == nil {
+		return fmt.Errorf("%s already exists", outPath)
+	}
+
+	file, err := os.Create(outPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for _, line := range lines {
+		_, err := file.WriteString(line + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- grep-tool/main.go
+++ grep-tool/main.go
@@ -1,109 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"bufio"
-	"flag"
-	"fmt"
-	"io"
-	"os"
-	"strings"
-)
+import "grep-cli/cmd"
 
 func main() {
-	const (
-		successCode int = 0
-		errorCode   int = 1
-	)
-
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) < 1 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <search_string> <filename>\n", os.Args[0])
-		os.Exit(errorCode)
-	}
-
-	searchString := args[0]
-
-	if len(args) == 1 {
-		lines, err := grepReader(searchString, os.Stdin)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
-			os.Exit(errorCode)
-		}
-		fmt.Println()
-		fmt.Println()
-		for _, line := range lines {
-			fmt.Printf("%s\n", line)
-		}
-	}
-
-	if len(args) == 2 {
-		filename := args[1]
-		file, err := validateFile(filename)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(errorCode)
-		}
-		defer file.Close()
-
-		matches, err := grepReader(searchString, file)
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %s: %v\n", os.Args[0], filename, err)
-			os.Exit(errorCode)
-		}
-
-		for _, matchedLines := range matches {
-			fmt.Println(matchedLines)
-		}
-	}
-
-	os.Exit(successCode)
-}
-
-func validateFile(filename string) (*os.File, error) {
-	file, err := os.Open(filename)
-
-	if err != nil {
-		if os.IsPermission(err) {
-			return nil, fmt.Errorf("%s: %s: Permission denied", os.Args[0], filename)
-		}
-		if os.IsNotExist(err) {
-			return nil, fmt.Errorf("%s: %s: open: No such file or directory", os.Args[0], filename)
-		}
-		return nil, fmt.Errorf("%s: %s: %v", os.Args[0], filename, err)
-	}
-
-	info, err := file.Stat()
-	if err != nil {
-		file.Close()
-		return nil, fmt.Errorf("%s: %s: %v", os.Args[0], filename, err)
-	}
-
-	if info.IsDir() {
-		file.Close()
-		return nil, fmt.Errorf("%s: %s: read: Is a directory", os.Args[0], filename)
-	}
-
-	return file, nil
-}
-
-func grepReader(searchString string, reader io.Reader) ([]string, error) {
-	var matches []string
-
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, searchString) {
-			matches = append(matches, line)
-		}
-	}
-
-	err := scanner.Err()
-	if err != nil {
-		return nil, err
-	}
-
-	return matches, nil
+	cmd.Execute()
 }
