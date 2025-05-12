971db5bda3b953b2abe51bea09b9eb619e3150f1
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-05-12 16:21:09 +0530

    main file updated

--- student-api/main.go
+++ student-api/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"log"
+	"student-api/internal/db"
 	"student-api/internal/handler"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables")
+	}
+
+	db.Connect()
+
 	r := gin.Default()
 	handler.RegisterRoutes(r)
+
 	r.Run(":8080")
 }
