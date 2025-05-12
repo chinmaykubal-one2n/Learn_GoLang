9faa1e1bbdf6416e428a7edabef7fa26ce8c8443
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-05-12 16:22:54 +0530

    model upgraded for db connectivity

--- student-api/internal/model/student.go
+++ student-api/internal/model/student.go
@@ -1,7 +1,7 @@
 package model
 
 type Student struct {
-	ID    string `json:"id"`
+	ID    string `gorm:"primaryKey" json:"id"`
 	Name  string `json:"name" binding:"required"`
 	Age   int    `json:"age" binding:"required,gte=1,lte=120"`
 	Email string `json:"email" binding:"required,email"`
