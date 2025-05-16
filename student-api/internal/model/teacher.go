a4ac1b45d1cdc29210088a2a277c220fe723916a
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-05-16 15:18:49 +0530

    teacher table changed

--- student-api/internal/model/teacher.go
+++ student-api/internal/model/teacher.go
@@ -2,7 +2,7 @@ package model
 
 type Teacher struct {
 	ID       string `gorm:"primaryKey" json:"id"`
-	Username string `json:"username" binding:"required"`
+	Username string `gorm:"uniqueIndex" json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
 	Role     string `json:"role" binding:"required"` // e.g., "admin", "regular", etc.
