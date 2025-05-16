cecff1addeccef90254e0c745bc861f4c916bf78
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-05-16 14:29:13 +0530

    password hashed

--- student-api/internal/service/teacher.go
+++ student-api/internal/service/teacher.go
@@ -5,10 +5,18 @@ import (
 	"student-api/internal/model"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func CreateTeacher(t model.Teacher) (model.Teacher, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(t.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return model.Teacher{}, err
+	}
+	t.Password = string(hashedPassword)
+
 	t.ID = uuid.New().String()
+
 	result := db.DB.Create(&t)
 	if result.Error != nil {
 		return model.Teacher{}, result.Error
