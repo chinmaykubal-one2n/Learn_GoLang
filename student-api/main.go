835021eae5b541f655db2d03ed7aedd53f63f536
chinmaykubal-one2n <chinmay.kubal@one2n.in>
2025-05-21 12:20:55 +0530

    graceful shutdown added for the server

--- student-api/main.go
+++ student-api/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"student-api/internal/db"
 	"student-api/internal/handler"
 	"student-api/internal/middleware"
 	"student-api/internal/service"
+	"syscall"
+	"time"
 
 	_ "student-api/docs"
 
@@ -62,5 +68,36 @@ func main() {
 		h.RegisterRoutes(api)
 	}
 
-	routerEngine.Run(":8080")
+	// https://gin-gonic.com/en/docs/examples/graceful-restart-or-stop/
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: routerEngine,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no params) by default sends syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Println("Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	<-ctx.Done()
+	log.Println("timeout of 5 seconds.")
+	log.Println("Server exiting")
 }
